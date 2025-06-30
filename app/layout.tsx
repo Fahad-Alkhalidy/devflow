f4849a0420e2de33650c02d0485d0e90a0a6f97d
Fahad-Alkhalidy <fahadkhalidy700@gmail.com>
2025-06-30 16:02:16 +0300

    Applied The dark & light modes, and created a nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import NavBar from "@/components/navigation/navbar";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -27,11 +29,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar></NavBar>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
