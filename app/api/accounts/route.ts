728de6681796bd819c7a40c1e7ee83aa7d4915ce
Fahad-Alkhalidy <fahadkhalidy700@gmail.com>
2025-07-25 19:11:02 +0300

    Implement Create & Read account routes

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+import Account from "@/database/account.model";
+
+export async function GET() {
+  try {
+    await dbConnect();
+    const accounts = await Account.find();
+    return NextResponse.json(
+      { success: true, data: accounts },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// Create User
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+
+    const validatedData = AccountSchema.parse(body); //safeParse if having a password
+
+    const existingAccount = await Account.findOne({
+      provider: validatedData.provider,
+      providerValidatedData: validatedData.providerAccountId,
+    });
+    if (existingAccount)
+      throw new ForbiddenError("Account with the same provider already exist!");
+    const newAccount = await Account.create(validatedData);
+
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
