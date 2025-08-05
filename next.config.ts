5673564edce3bc1cf780e3d5b47c35ebc2ee4483
Fahad-Alkhalidy <fahadkhalidy700@gmail.com>
2025-08-05 14:13:29 +0300

    Implement logout and add the user avatar on the nav bar

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "static.vecteezy.com", port: "" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com", port: "" },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+      },
     ],
   },
 };
