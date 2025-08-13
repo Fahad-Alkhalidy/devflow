ca3623ec84bc22e23b7826d77bf65aa84e980571
Fahad-Alkhalidy <fahadkhalidy700@gmail.com>
2025-08-13 18:47:38 +0300

    Implemented Tags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,15 @@
+import { getTags } from "@/lib/actions/tag.action";
 import React from "react";
 
-const Tags = () => {
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    filter: "",
+    query: "",
+  });
+  const { tags } = data || {};
+  console.log("Tags:", tags);
   return <div></div>;
 };
 
