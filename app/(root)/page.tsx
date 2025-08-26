6c9c33ae225abca942ef27bbcea353f0e0f4059b
Fahad-Alkhalidy <fahadkhalidy700@gmail.com>
2025-08-27 00:27:28 +0300

    Implement Pagination Everyware

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import QuesionCard from "@/components/cards/QuestionCard";
 import DataRenderer from "@/components/DataRenderer";
 import CommonFilter from "@/components/filters/CommonFilter";
 import HomeFilter from "@/components/filters/HomeFilter";
+import Pagination from "@/components/Pagination";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
@@ -29,7 +30,7 @@ const Home = async ({ searchParams }: SearchParams) => {
     filter: filter || "",
     query: query || "",
   });
-  const { questions } = data || {};
+  const { questions, isNext } = data || {};
   return (
     <>
       <section
@@ -71,6 +72,7 @@ const Home = async ({ searchParams }: SearchParams) => {
           </div>
         )}
       />
+      <Pagination page={page} isNext={isNext || false} />
 
       {/* {success ? (
         <div className="mt-10 flex w-full flex-col gap-6">
