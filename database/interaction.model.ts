10630664dd548eec15c30e24ef2b795d4af3755f
Fahad-Alkhalidy <fahadkhalidy700@gmail.com>
2025-09-02 15:37:52 +0300

    Implemented Interaction And Reputation Logic

--- database/interaction.model.ts
+++ database/interaction.model.ts
@@ -4,17 +4,28 @@ export interface IInteraction {
   user: Types.ObjectId; // Reference to User ID
   action: string; // e.g., 'like', 'dislike', etc.
   actionId: Types.ObjectId; // Reference to the entity being interacted with (e.g., Question, Answer)
-  actionType: "Question" | "Answer"; // Type of entity being interacted with (e.g., 'Question', 'Answer')
+  actionType: "question" | "answer"; // Type of entity being interacted with (e.g., 'Question', 'Answer')
 }
+
+export const InteractionActionEnums = [
+  "view",
+  "upvote",
+  "downvote",
+  "bookmark",
+  "post",
+  "edit",
+  "delete",
+  "search",
+] as const;
 
 export interface IInteractionDocument extends IInteraction, Document {}
 
 const InteractionSchema = new Schema<IInteraction>(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    action: { type: String, required: true }, // e.g., 'like', 'dislike', etc.
+    action: { type: String, enum: InteractionActionEnums, required: true }, // e.g., 'like', 'dislike', etc.
     actionId: { type: Schema.Types.ObjectId, required: true }, // Reference to the entity being interacted with (e.g., Question, Answer)
-    actionType: { type: String, enum: ["Question", "Answer"], required: true }, // Type of entity being interacted with
+    actionType: { type: String, enum: ["question", "answer"], required: true }, // Type of entity being interacted with
   },
   { timestamps: true }
 );
