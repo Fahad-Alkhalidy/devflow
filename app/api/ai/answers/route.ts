b1d2a2b7edcd485744c3e600f50f76d82347a738
Fahad-Alkhalidy <fahadkhalidy700@gmail.com>
2025-09-23 16:33:25 +0300

    Modification

--- app/api/ai/answers/route.ts
+++ app/api/ai/answers/route.ts
@@ -28,7 +28,7 @@ export async function POST(req: Request) {
       Prioritize the user's answer only if it's correct. If it's incomplete or incorrect, improve or correct it while keeping the response concise and to the point. 
       Provide the final answer in markdown format.`,
       system:
-        "You are a helpful assistant that provides informative responses in markdown format. Use appropriate markdown syntax for headings, lists, code blocks, and emphasis where necessary. For code blocks, use short-form smaller case language identifiers (e.g., 'js' for JavaScript, 'py' for Python, 'ts' for TypeScript, 'html' for HTML, 'css' for CSS, etc.).",
+        "You are a helpful assistant that provides informative responses in markdown format. Use appropriate markdown syntax for headings, lists, code blocks, and emphasis where it is necessary. For code blocks, use short-form smaller case language identifiers (e.g., 'js' for JavaScript, 'py' for Python, 'ts' for TypeScript, 'html' for HTML, 'css' for CSS, etc.).",
     });
     return NextResponse.json(
       { success: true, data: text },
